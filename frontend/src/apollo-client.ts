c36bbcb58061e4a64cc276fc3110057dcd35dc76
nataliia-bratchykova <nataliya.bratchykova@gmail.com>
2026-03-13 18:01:23 +0200

    - added header for preventing issue with not uploading image

--- frontend/src/apollo-client.ts
+++ frontend/src/apollo-client.ts
@@ -3,6 +3,9 @@ import { ApolloProvider } from '@apollo/client/react';
 import  createUploadLink  from 'apollo-upload-client/UploadHttpLink.mjs';
 const httpLink = new createUploadLink({
   uri: 'http://localhost:3000/graphql',
+  headers: {
+    'x-apollo-operation-name': 'UploadProfileImage',
+  },
 });
 
 const client = new ApolloClient({
